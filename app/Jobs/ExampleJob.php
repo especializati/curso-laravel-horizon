c2e2dcf221de8e6e9432737aca52554b783bc0ba
Carlos Ferreira <carlos@especializati.com.br>
2022-07-26 15:19:12 -0300

    Create Example JOB

---
+++ app/Jobs/ExampleJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class ExampleJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        protected array $payload
+    ) {}
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::info('Start ExampleJob');
+        sleep(10);
+        Log::info('End ExampleJob');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
+use App\Jobs\ExampleJob;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/test-job', function () {
+    ExampleJob::dispatch(['example' => 'value']);
+
+    return 'ok';
+});
 
 Route::get('/', function () {
     return view('welcome');
