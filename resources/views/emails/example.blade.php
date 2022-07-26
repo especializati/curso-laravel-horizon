9def46208a94519ceb15f9e3de1770a2b67f47b6
Carlos Ferreira <carlos@especializati.com.br>
2022-07-26 16:05:33 -0300

    Send Mail With Queue

---
+++ resources/views/emails/example.blade.php
@@ -0,0 +1,1 @@
+Example E-mail with Queue
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use App\Jobs\ExampleJob;
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/test-mail', function () {
+    Mail::to('carlos@especializati.com.br')
+            ->send(new TestMail);
+
+    return 'ok - mail sended';
+});
 
 Route::get('/test-job', function () {
     ExampleJob::dispatch(['example' => 'value']);
